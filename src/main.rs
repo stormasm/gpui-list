39fb5192a991c881d2e4a61ab2b653bcafa73c73
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2024-02-27 10:09:14 -0800

    remove global_list

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 mod common;
-mod global_list;
 mod simple_list;
 
 use gpui::*;
@@ -7,5 +6,4 @@ use gpui::*;
 fn main() {
     let app = App::new();
     simple_list::run_app(app);
-    // global_list::run_app(app);
 }
